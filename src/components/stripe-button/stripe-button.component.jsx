fca960d2508b432aeb4899682b45ce1e9dfe77ce
roshanb <roshan.birwatkar@gmail.com>
2020-09-20 00:20:56 +0530

    forked striped version of app added firebase and stripe key

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const priceForStripe = price * 100; //Tried_stripe_need_price_in_cents
+  const publishableKey =
+    "pk_test_51HTAtuCGg28WI1h5RBL3fiM9R1KYQsMYJzGxsV32x89N8RawLRjV9T2AJIfzPoKA7lzEOnGy4d0NO4oKHmOSeN6P00bbxxBNuK";
 
-  const onToken = token => {
+  const onToken = (token) => {
     console.log(token);
-    alert('Payment Succesful!');
+    alert("Payment Succesful!");
   };
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,16 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyAkaiPTWkGrQvATLr6uq1FuufmiU2VFmCk",
+  authDomain: "ztm-ecomm.firebaseapp.com",
+  databaseURL: "https://ztm-ecomm.firebaseio.com",
+  projectId: "ztm-ecomm",
+  storageBucket: "ztm-ecomm.appspot.com",
+  messagingSenderId: "109023820095",
+  appId: "1:109023820095:web:064cf6511ab1a12f2d3b8a",
+  measurementId: "G-JDCCPJCZ9M",
 };
 
 firebase.initializeApp(config);
@@ -29,10 +30,10 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         displayName,
         email,
         createdAt,
-        ...additionalData
+        ...additionalData,
       });
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message);
     }
   }
 
@@ -43,7 +44,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' });
+provider.setCustomParameters({ prompt: "select_account" });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
